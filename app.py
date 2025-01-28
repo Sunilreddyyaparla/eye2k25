55b6bc59b72912e1319ffe84420b1d88a3a098c8
Yaparla Sunil Kumar Reddy <161870912+Sunilreddyyaparla@users.noreply.github.com>
2025-01-28 22:26:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ def payment_callback():
                 payment_id=payment_data['razorpay_payment_id'],
                 name=registration_data['fullname'],
                 email=registration_data['email'],
-                mobileno=int(registration_data['mobile']),
+                mobileno=registration_data['mobile'],
                 event=registration_data['event_name'],
                 college=registration_data['yourcollege']
             )
